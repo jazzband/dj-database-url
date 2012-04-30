a6f73aca04c3c4267dcf11a5beb63740ef66a68a
Kenneth Reitz <me@kennethreitz.com>
2012-04-30 13:43:46 -0400

    lawyer up

---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+dj-database-url
+~~~~~~~~~~~~~~~
+
+This simple Django utility allows you to utilize the
+`12factor <http://www.12factor.net/backing-services>`_ inspired
+``DATABASE_URL`` environment variable to configure your Django application.
+
+
+Usage
+-----
+
+Configure your database in ``settings.py``::
+
+    DATABASES = dj_database_url.config(DATABASES)
+
+Nice and simple.
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='dj-database-url',
+    version='0.1.0',
+    url='https://github.com/kennethreitz/dj-database-url',
+    license='BSD',
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    description='Use Database URLs in your Django Application.',
+    long_description=__doc__,
+    py_modules=['dj_database_url'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['Flask'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)

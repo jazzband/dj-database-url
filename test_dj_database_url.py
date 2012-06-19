f39a80e12a2d3ced43e5a7f971443e1f69909805
Kenneth Reitz <me@kennethreitz.com>
2012-06-19 11:09:00 -0400

    postgres test

--- test_dj_database_url.py
+++ test_dj_database_url.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
+import os
 import unittest
+
+import dj_database_url
+
+
+POSTGIS_URL = 'postgis://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn'
 
 
 class DatabaseTestSuite(unittest.TestCase):
@@ -9,9 +15,20 @@ class DatabaseTestSuite(unittest.TestCase):
     def test_truth(self):
         assert True
 
+    def test_postgres_parsing(self):
+        url = 'postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn'
+        url = dj_database_url.parse(url)
+
+        assert url['ENGINE'] == 'django.db.backends.postgresql_psycopg2'
+        assert url['NAME'] == 'd8r82722r2kuvn'
+        assert url['HOST'] == 'ec2-107-21-253-135.compute-1.amazonaws.com'
+        assert url['USER'] == 'uf07k1i6d8ia0v'
+        assert url['PASSWORD'] == 'wegauwhgeuioweg'
+        assert url['PORT'] == 5431
+
 
 # unittest.TestCase
-# postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn
+#
 
 
 if __name__ == '__main__':

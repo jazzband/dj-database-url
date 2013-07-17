de1545cdf8f71026a447fefdaca4ee43d791f7ad
Kenneth Reitz <me@kennethreitz.com>
2013-07-17 16:39:48 -0400

    v0.2.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ from setuptools import setup
 
 setup(
     name='dj-database-url',
-    version='0.2.1',
+    version='0.2.2',
     url='https://github.com/kennethreitz/dj-database-url',
     license='BSD',
     author='Kenneth Reitz',

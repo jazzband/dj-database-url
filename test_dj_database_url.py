14983254d7bc3aeb48ae0553ca69b060bc917b8c
Justin Poehnelt <justin.poehnelt@gmail.com>
2016-09-14 17:34:38 -0600

    remove extraneous test

--- test_dj_database_url.py
+++ test_dj_database_url.py
@@ -101,22 +101,6 @@ class DatabaseTestSuite(unittest.TestCase):
         assert url['PASSWORD'] == '69772142'
         assert url['PORT'] == ''
 
-    def test_database_url(self):
-        del os.environ['DATABASE_URL']
-        a = dj_database_url.config()
-        assert not a
-
-        os.environ['DATABASE_URL'] = 'postgres://uf07k1i6d8ia0v:wegauwhgeuioweg@ec2-107-21-253-135.compute-1.amazonaws.com:5431/d8r82722r2kuvn'
-
-        url = dj_database_url.config()
-
-        assert url['ENGINE'] == 'django.db.backends.postgresql_psycopg2'
-        assert url['NAME'] == 'd8r82722r2kuvn'
-        assert url['HOST'] == 'ec2-107-21-253-135.compute-1.amazonaws.com'
-        assert url['USER'] == 'uf07k1i6d8ia0v'
-        assert url['PASSWORD'] == 'wegauwhgeuioweg'
-        assert url['PORT'] == 5431
-
     def test_empty_sqlite_url(self):
         url = 'sqlite://'
         url = dj_database_url.parse(url)

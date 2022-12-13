ad0645cb5977af50ed048f7cc227eff03c4558f0
Matt Seymour <mattaseymour@gmail.com>
2022-12-13 09:18:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme = Path("README.rst").read_text()
 
 setup(
     name="dj-database-url",
-    version="1.1.0",
+    version="1.2.0",
     url="https://github.com/jazzband/dj-database-url",
     license="BSD",
     author="Original Author: Kenneth Reitz, Maintained by: JazzBand Community",
